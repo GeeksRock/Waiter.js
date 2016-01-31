113c12232eba3ce1b21ba8d80d24bcc2bd4ee306
Michelle Edmondson <GeeksRock@users.noreply.github.com>
2016-01-31 05:45:51 -0800

    Update waiter.js
    Added additional error-checking to Waiter.takeDataOrder

--- waiter.js
+++ waiter.js
@@ -43,24 +43,29 @@ var Waiter = (function () {
 
     return {
         takeDataOrder: function (data, records_per_page) {
-            var data_row = undefined, count = 0, next_page_start = 0, next_page_end = Number(records_per_page);
-            this.data = data;
-            this.recordsPerPage = Number(records_per_page);
-            this.numberOfPages = Math.max(Math.ceil(Number(this.data.length / this.recordsPerPage)));
-
-            if (this.numberOfPages > 0) {
-                this.pages = [];
+            var data_row = undefined, count = 0, next_page_start = 0, next_page_end = undefined;//Number(records_per_page)
+            
+            if (data !== null || data !== undefined || data.length > 0) {
+               this.data = data; 
+            } else {
+                if (records_per_page !== null || records_per_page !== undefined || !isNaN(records_per_page)) {
+                    this.recordsPerPage = Number(records_per_page);
+                } 
+                this.numberOfPages = Math.max(Math.ceil(Number(this.data.length / this.recordsPerPage)));
+                
+                if (this.numberOfPages > 0) {
+                    this.pages = [];
+                }
+                
+                while (count < this.numberOfPages) {
+                    this.pages.push(this.data.slice(next_page_start, next_page_end));
+                    count++;
+                    next_page_start += Number(this.recordsPerPage);
+                    next_page_end += Number(this.recordsPerPage);
+                }
+                
+                this.currentPage = this.pages[0];
             }
-
-            while (count < this.numberOfPages) {
-                this.pages.push(this.data.slice(next_page_start, next_page_end));
-                count++;
-                next_page_start += Number(this.recordsPerPage);
-                next_page_end += Number(this.recordsPerPage);
-            }
-
-            this.currentPage = this.pages[0];
-
             return this;
         },
         takePageOrder: function (page_number, criteria, direction) {
